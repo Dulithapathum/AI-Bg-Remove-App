be1e0c544c4bf563a95259950c4e730efb3a292b
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-20 15:17:35 +0530

    Header Section Create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Result } from "./pages/Result";
 import { BuyCredits } from "./pages/BuyCredits";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-slate-50">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/buy" element={<BuyCredits />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Header } from "../components/Header";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      Home
+    </div>
+  );
 };
