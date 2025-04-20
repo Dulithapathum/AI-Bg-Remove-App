1adfbc8a095ded0da322468c03c5b5a81e7fac9c
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-20 20:40:38 +0530

    Connect DB

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
+import connectDB from "./configs/mongodb.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+await connectDB();
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT} `);
 });
