2b6ada9e179af459088bd6ce3cb823a8d08f156e
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-20 20:28:18 +0530

    Build Server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT} `);
+});
