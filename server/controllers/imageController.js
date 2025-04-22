5540684991777685b740d4cf63c2c0926f22f635
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-22 08:25:24 +0530

    Create multer funtion

---
+++ server/controllers/imageController.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import fs from "fs";
+import FormData from "form-data";
+
+const removeBgImage = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(" Error:", error.message);
+  }
+};
+
+export { removeBgImage };
